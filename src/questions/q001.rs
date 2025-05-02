28f19cc0d08c42d86ab41cc5a8b8df23f468385f
kyochanpy <nasu.9618.0527@gmail.com>
2025-05-02 15:32:44 +0900

    fix: 答えの画像を比較して答え合わせするように変更

--- src/questions/q001.rs
+++ src/questions/q001.rs
@@ -1,30 +1,27 @@
-use image::{Rgb, RgbImage, GenericImageView};
+use image::{DynamicImage, GenericImageView, Rgb, RgbImage};
 
-pub fn run(base_image_path: &str, result_image_path: &str) {
-    // 画像読み込み
+pub fn rgb_to_bgr(base_image_path: &str) -> DynamicImage {
     let img = image::open(base_image_path).unwrap();
-
-    // 新しい画像を作成
     let mut result_img = RgbImage::new(img.width(), img.height());
 
-    // ピクセル単位で処理
     for (x, y, pixel) in result_img.enumerate_pixels_mut() {
         let rgb = img.get_pixel(x, y);
         *pixel = Rgb([rgb[2], rgb[1], rgb[0]]);
     }
-
-    // 保存
-    result_img.save(result_image_path).unwrap();
+    DynamicImage::ImageRgb8(result_img)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::questions::test_image_processing;
 
     #[test]
     fn test_run() {
-        let base_image_path = "src/images/imori.jpg";
-        let result_image_path = "src/images/result001.jpg";
-        run(base_image_path, result_image_path);
+        test_image_processing(
+            rgb_to_bgr,
+            "src/images/imori.png",
+            "src/images/answers/answer_001.png",
+        );
     }
 }
