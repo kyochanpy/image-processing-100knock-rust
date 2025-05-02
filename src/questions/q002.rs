ded6aa8bec5c0d842958625fc88ff9a17f013b5b
kyochanpy <nasu.9618.0527@gmail.com>
2025-05-02 16:22:13 +0900

    fix: format

--- src/questions/q002.rs
+++ src/questions/q002.rs
@@ -7,7 +7,7 @@ pub fn rgb_to_gray(base_image_path: &str) -> DynamicImage {
     for (x, y, pixel) in result_img.enumerate_pixels_mut() {
         let pix = img.get_pixel(x, y);
         let gray = Luma([
-            (pix[0] as f32 * 0.2126 + pix[1] as f32 * 0.7152 + pix[2] as f32 * 0.0722) as u8
+            (pix[0] as f32 * 0.2126 + pix[1] as f32 * 0.7152 + pix[2] as f32 * 0.0722) as u8,
         ]);
         *pixel = gray;
     }
